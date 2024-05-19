a4c2746e934105feb83d840cde0204eaca24be91
nthskyradiated <andy.pandaan@outlook.com>
2024-05-19 22:21:15 +0400

    working on single project

--- src/routes/protected/project/[ID]/+page.server.ts
+++ src/routes/protected/project/[ID]/+page.server.ts
@@ -1,12 +1,24 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import projects from '@/db/schema/projectsSchema/projects';
+import db from '@/db';
+import { eq } from 'drizzle-orm';
 
 export const load = (async ({ locals, params }) => {
 	if (!locals.user) {
 		redirect(302, '/');
 	}
+
 	const { ID } = params;
+	const projectId = Number(ID)
+	const project = await db.select().from(projects).where(eq(projects.id, projectId));
+	
+	if (!project) {
+		return new Error('Project not found');
+	}
+
 	return {
-		ID
+		ID,
+		project: project[0]
 	};
 }) satisfies PageServerLoad;
--- src/routes/protected/project/[ID]/+page.svelte
+++ src/routes/protected/project/[ID]/+page.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
+	import Card from '@/lib/components/ui/card/card.svelte';
+	import Button from '@/lib/components/ui/button/button.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	const {project} = data
+	
 </script>
 
-<h1>Project Page</h1>
+
+<Card class="my-2 w-2/3 p-6">
+	<div class="flex flex-col gap-4">
+		<h1 class="text-3xl font-bold mb-2 ">{project?.name}</h1>
+		<hr class="mb-8">
+		<div class='flex justify-between px-4'><span class="font-bold">Project Details: </span><p class="inline pl-12">{project?.details}</p></div>
+		<div class='flex justify-between px-4'><span class="font-bold">Starting Balance: </span><p class="inline pl-12">{project?.startingFunds}</p></div>
+		<div class='flex justify-between px-4'><span class="font-bold">Current Balance: </span><p class="inline pl-12">{project?.totalFunds}</p></div>
+		<div class='flex justify-between px-4'><span class="font-bold">Updated At: </span><p class="inline pl-12">{project?.updatedAt}</p></div>
+		<div class='flex justify-between px-4'><span class="font-bold">Date Created: </span><p class="inline pl-12">{project?.createdAt}</p>
+		</div>
+	</div>
+	<div class="flex gap-12 justify-center text-center mt-12 w-auto m-auto">
+		<Button variant={'outline'}>Add transaction</Button>
+		<Button variant={'outline'}>Update transaction</Button>
+		<Button variant={'destructive'}>Remove transaction</Button>
+	</div>
+</Card>
