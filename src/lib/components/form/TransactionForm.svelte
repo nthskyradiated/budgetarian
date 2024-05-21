b3036d6d2188e2fe5633cf6e2968d3c45a447782
nthskyradiated <andy.pandaan@outlook.com>
2024-05-21 16:57:03 +0400

    working on reactivity

--- src/lib/components/form/TransactionForm.svelte
+++ src/lib/components/form/TransactionForm.svelte
@@ -9,21 +9,23 @@
 	import { zod } from 'sveltekit-superforms/adapters';
 	import { toast } from 'svelte-sonner';
 	import { TransactionZodSchema, type transactionZodSchema } from '@/lib/zodValidators/zodProjectValidation';
-
-	import { maxNameLen, minNameLen } from '@/lib/zodValidators/zodParams';
+	import SuperDebug from 'sveltekit-superforms';
+	import { maxNameLen, minNameLen, EXPENSES_CATEGORIES, INFLOWS_CATEGORIES } from '@/lib/zodValidators/zodParams';
 	import { Label } from '../ui/label';
-	
+	import { Switch } from "$lib/components/ui/switch/index"
+	import * as Select from "$lib/components/ui/select"
 	export let formData: SuperValidated<transactionZodSchema>;
 	export let formAction: string;
 	export let dialogTitle : string
 	export let dialogDescription : string
 
+	
 
 	const { enhance, form, errors, message, delayed } = superForm(formData, {
 		resetForm: true,
 		taintedMessage: null,
 		validators: zod(TransactionZodSchema),
-		dataType: "form",
+		dataType: "json",
 
 		onUpdated: () => {
 			if (!$message) return;
@@ -39,7 +41,33 @@
 			}
 		}
 	});
+
+
+
+
+
+
+	let selectedTransactionType =$form.transactionType.transactionType
+	let categoriesValues = selectedTransactionType === 'income' ? INFLOWS_CATEGORIES.options : EXPENSES_CATEGORIES.options;
+
+	//@todo
+	$: {
+    if ($form.transactionType.transactionType === 'income') {
+        categoriesValues = [...INFLOWS_CATEGORIES.options];
+    } else if ($form.transactionType.transactionType === 'expenses') {
+        categoriesValues = [...EXPENSES_CATEGORIES.options];
+    }
+}
+
+
+function handleTransactionTypeChange(event: 'income' | 'expenses') {
+	console.log(event)
+	return selectedTransactionType = event;
+}
+
+$: {selectedTransactionType}
 </script>
+<SuperDebug data={$form} />
 
 <div class="my-8 flex flex-wrap justify-between gap-4">
 	<Dialog.Root>
@@ -74,22 +102,40 @@
 					step='0.01'
 					bind:value={$form.amount}
 					errorMessage={$errors.amount}
-					maxlength={maxNameLen}
-					minlength={minNameLen}
 				/>
+
+				<Switch id="recurring" bind:checked={$form.isRecurring} />
+				<Label for="isRecurring"> Toggle Recurring</Label>
 
 				<RadioGroup.Root bind:value={$form.transactionType.transactionType} class="grid gap-2" {...$$restProps}>
 					<div class="flex items-center space-x-2">
-					  <RadioGroup.Item value="income" id="r1" />
+					  <RadioGroup.Item value="income" id="r1" on:click={() => handleTransactionTypeChange('income')}/>
 					  <Label for="r1">inflow</Label>
 					</div>
 					<div class="flex items-center space-x-2">
-					  <RadioGroup.Item value="expense" id="r2" />
+					  <RadioGroup.Item value="expense" id="r2" on:click={() => handleTransactionTypeChange('expenses')}/>
 					  <Label for="r2">expense</Label>
 					</div>
 					<RadioGroup.Input name="spacing" />
 				  </RadioGroup.Root>
-
+				
+				  <Select.Root>
+					<Select.Trigger class="w-[180px]">
+						<Select.Value placeholder="Category" />
+					</Select.Trigger>
+					<Select.Content>
+						{#if selectedTransactionType === 'income'}
+							{#each categoriesValues as category}
+								<Select.Item value={category}>{category}</Select.Item>
+							{/each}
+						{:else if selectedTransactionType === 'expenses'}
+							{#each categoriesValues as category}
+								<Select.Item value={category}>{category}</Select.Item>
+							{/each}
+						{/if}
+					</Select.Content>
+					<input hidden bind:value={$form.transactionType.categories} name="categories"/>
+				</Select.Root>
 				<SubmitButton disabled={$delayed}>Add Transaction</SubmitButton>
 			</form>
 		</Dialog.Content>
